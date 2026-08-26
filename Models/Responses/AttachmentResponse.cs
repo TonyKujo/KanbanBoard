e7b5f6c46609cfd46bdc26222f373f3e1d898f0f
TonyKujo <raidert21@gmail.com>
2026-08-26 21:44:43 +0300

    Новая миграция
    Создана новая миграция, DTO под вложения и начало сервиса для вложений. С этого момента нужно будет сделать сервис и контроллер вложений, после чего провести тесты приложения, выявить баги и исправить их. До выходных всё будет готово

---
+++ Models/Responses/AttachmentResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace KanbanBoard.Models.Responses
+{
+    public class AttachmentResponse
+    {
+        public int AttachmentId { get; set; }
+        public string FileName { get; set; } = null!;
+        public string FilePath { get; set; } = null!;
+        public DateTime DateOfUpload { get; set; }
+        public UserResponse Uploader { get; set; } = null!;
+    }
+}
---
+++ Services/AttachmentService.cs
@@ -0,0 +1,27 @@
+﻿using KanbanBoard.Data;
+using KanbanBoard.Models;
+using KanbanBoard.Models.Requests;
+using KanbanBoard.Models.Responses;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata.Conventions;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Services
+{
+    public class AttachmentService
+    {
+        private readonly KanbanBoardDbContext _db;
+
+        public AttachmentService(KanbanBoardDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        private async Task<bool> IsUserBoardMemberAsync(int boardId, int userId, CancellationToken ct)
+        {
+            return await _db.BoardUsers.AnyAsync(bu => bu.BoardId == boardId && bu.UserId == userId, ct);
+
+
+        }
+    }
+}
