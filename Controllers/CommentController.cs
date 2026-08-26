064da8fe34f319cc388c2751b66232e5e85de998
TonyKujo <raidert21@gmail.com>
2026-08-26 15:45:06 +0300

    Добавлен контроллер комментариев
    Добавлены соответствующие сервисы, контроллеры и DTO

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,36 @@
+﻿using KanbanBoard.Models.Requests;
+using KanbanBoard.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+namespace KanbanBoard.Controllers
+{
+    [Authorize]
+    public class CommentController(CommentService commentService) : Controller
+    {
+        private readonly CommentService _commentService = commentService;
+
+        private int GetUserId()
+        {
+            var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(userIdClaim!);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Route("api/boards/{boardId}/tasks/{taskId}/comments")]
+        public async Task<IActionResult> GetTaskComments(int boardId, int taskId, CancellationToken ct)
+        {
+            var userId = GetUserId();
+
+            var result = await _commentService.GetAllCommentsOfTaskAsync(boardId, userId, taskId, ct);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Services/CommentService.cs
@@ -0,0 +1,45 @@
+﻿using KanbanBoard.Data;
+using KanbanBoard.Models.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace KanbanBoard.Services
+{
+    public class CommentService
+    {
+        private readonly KanbanBoardDbContext _db;
+
+        public CommentService(KanbanBoardDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        private async Task<bool> IsUserBoardMemberAsync(int boardId, int userId, CancellationToken ct)
+        {
+            return await _db.BoardUsers.AnyAsync(bu => bu.BoardId == boardId && bu.UserId == userId, ct);
+
+
+        }
+
+        public async Task<List<CommentResponse>?> GetAllCommentsOfTaskAsync(int boardId, int userId, int taskId, CancellationToken ct)
+        {
+            if(! await IsUserBoardMemberAsync(boardId, userId, ct))
+            {
+                return null;
+            }
+
+            var comments = await _db.Comments
+                .Where(c => c.TaskId == taskId)
+                .Select(c => new CommentResponse
+                {
+                    Author = new UserResponse { Login = c.Author.User.Login, UserId = c.Author.UserId },
+                    CommentId = c.CommentId,
+                    MadeDate = c.DateOfMade,
+                    Text = c.Text
+
+                })
+                .ToListAsync(ct);
+
+            return comments;
+        }
+    }
+}
