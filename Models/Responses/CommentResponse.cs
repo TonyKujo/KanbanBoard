3d6bc69370a37727fa8febe4f4317ac0742af8b4
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2026-08-30 13:27:23 +0300

    Бек: CommentResponse отдаёт задачу и вложения комментария

--- Models/Responses/CommentResponse.cs
+++ Models/Responses/CommentResponse.cs
@@ -1,11 +1,13 @@
-﻿namespace KanbanBoard.Models.Responses
+namespace KanbanBoard.Models.Responses
 {
     public class CommentResponse
     {
         public int CommentId { get; set; }
-        public string Text { get; set; } = null!;
+        public int TaskId { get; set; }
+        public string? Text { get; set; }
         public DateTime MadeDate { get; set; }
         public bool IsEdited { get; set; }
         public UserResponse Author { get; set; } = null!;
+        public List<AttachmentResponse> Attachments { get; set; } = new List<AttachmentResponse>();
     }
 }
